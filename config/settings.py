2da0e8d736bf12b20ac4aa687694cdef105ee26f
yeonseo <yulga_nara@naver.com>
2020-06-19 12:55:17 +0900

    django-admin.py migrate을 위한 셋팅 / 시크릿 키 환경변수 설정

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l%f$cesl(k&%5&p8(+=nqu_o9qax4ic@ec^etek7(g*egn6a(v'
+SECRET_KEY = os.environ.get("Pu+@f]6N~\gGNtRtmJ:Q")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG"))
