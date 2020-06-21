11c421d6de78412945ea2dfb0309d4d21e30b520
yeonseo <yulga_nara@naver.com>
2020-06-21 13:44:28 +0900

    이미지 넣음 / 컨설팅 폼 넣음

--- consultings/views.py
+++ consultings/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django import forms
+from .models import Post
 
-# Create your views here.
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
